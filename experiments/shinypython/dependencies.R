8ef3723b1785d11fd65a78229edf4789fe354c32
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-01-26 17:21:15 +0100

    Added package dependencies Python

--- experiments/shinypython/dependencies.R
+++ experiments/shinypython/dependencies.R
@@ -1,6 +1,7 @@
 required_packages <- c(
   "shiny",
-  "reticulate"
+  "reticulate",
+  "here"
 )
 
 missing_packages <- required_packages[!(required_packages %in% installed.packages()[, "Package"])]
@@ -9,3 +10,26 @@ if (length(missing_packages) > 0) {
 }
 
 invisible(lapply(missing_packages, library, character.only = TRUE))
+
+library(reticulate)
+
+venv_name <- "r-reticulate"
+
+if(!virtualenv_exists(venv_name)) {
+  virtualenv_create(venv_name)
+  cat("Virtual environment created: ", venv_name, "\n")
+}
+
+library(here)
+
+virtualenv_install(
+  venv_name, 
+  packages = c(
+    "torch", 
+    "torchvision", 
+    "torchaudio"
+  ),
+  pip_options = "--verbose"
+)
+
+use_virtualenv(venv_name, required = TRUE)
